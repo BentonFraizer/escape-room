d45d5a3e5792f2929e3902abfca99760a9205501
Yuri Lopukhin <FOL2203012@yandex.ru>
2022-09-09 15:33:47 +0300

    Реализовано изменение стилей активных вкладок на панели навигации - при переходе между страницами Квесты и Контакты подсвечивается оранжевым активная вкладка

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -3,15 +3,30 @@ import * as S from './header.styled';
 import { changeType } from '../../../store/site-process/site-process';
 import { useAppDispatch } from '../../../hooks';
 import { TYPES } from '../../../consts';
+import { useEffect, useState } from 'react';
+import { AppRoute } from '../../../consts';
 
 const Header = () => {
   const dispatch = useAppDispatch();
+  const [isActiveLinkQuests, setIsActiveLinkQuests] = useState<boolean>(true);
+  const [isActiveLinkContacts, setIsActiveLinkContacts] = useState<boolean>(false);
+  const pathName = window.location.pathname;
+
+  useEffect(() => {
+    if (pathName === '/') {
+      setIsActiveLinkQuests(true);
+      setIsActiveLinkContacts(false)
+    } else {
+      setIsActiveLinkQuests(false);
+      setIsActiveLinkContacts(true);
+    }
+  }, [pathName]);
 
   return (
     <S.StyledHeader>
       <S.HeaderWrapper>
         <S.LogoLink
-          to="/"
+          to={AppRoute.Main}
           onClick={() => {
             dispatch(changeType(TYPES[0].en.toLowerCase()));
           }}
@@ -23,8 +38,8 @@ const Header = () => {
           <S.Links>
             <S.LinkItem>
               <S.Link
-                isActiveLink
-                to="/"
+                $isActiveLink={isActiveLinkQuests}
+                to={AppRoute.Main}
                 onClick={() => {
                   dispatch(changeType(TYPES[0].en.toLowerCase()));
                 }}
@@ -34,19 +49,23 @@ const Header = () => {
             </S.LinkItem>
 
             <S.LinkItem>
-              <S.Link to="#">Новичкам</S.Link>
+              <S.Link to="#" $isActiveLink={false}>Новичкам</S.Link>
             </S.LinkItem>
 
             <S.LinkItem>
-              <S.Link to="#">Отзывы</S.Link>
+              <S.Link to="#" $isActiveLink={false}>Отзывы</S.Link>
             </S.LinkItem>
 
             <S.LinkItem>
-              <S.Link to="#">Акции</S.Link>
+              <S.Link to="#" $isActiveLink={false}>Акции</S.Link>
             </S.LinkItem>
 
             <S.LinkItem>
-              <S.Link to="/contacts">Контакты</S.Link>
+              <S.Link
+                $isActiveLink={isActiveLinkContacts}
+                to={AppRoute.Contacts}
+                >
+              Контакты</S.Link>
             </S.LinkItem>
           </S.Links>
         </S.Navigation>
